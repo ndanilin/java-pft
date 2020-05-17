7146628299a599923cab3159eb23c7ed7e29b98b
ndanilin <ndanilin@solit-clouds.ru>
2020-05-17 16:13:40 +0300

    добавил ответ на Задание №3

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.annotations.Test;
+import ru.stqa.pdf.sandbox.Point;
+
+import static org.testng.Assert.assertEquals;
+
+public class PointTest {
+
+    @Test
+    public void testDistanceOne() {
+        Point point = new Point(1, 1);
+        assertEquals(point.distance(3, 3), 2.8284271247461903);
+    }
+
+    @Test
+    public void testDistanceTwo() {
+        Point point = new Point(-1, 8);
+        assertEquals(point.distance(7, -2), 12.806248474865697);
+    }
+}
