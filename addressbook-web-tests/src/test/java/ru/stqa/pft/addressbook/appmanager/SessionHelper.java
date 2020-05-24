e7469d2f25f9cb8ee7c21d4fc88f71d7986864ab
ndanilin <ndanilin@solit-clouds.ru>
2020-05-24 19:12:26 +0300

    создание HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -3,20 +3,19 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SessionHelper {
-    private WebDriver wd;
+public class SessionHelper extends HelperBase {
 
     public SessionHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void login(String username, String password) {
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.cssSelector("[value=Login]")).click();
+        type(By.name("user"), username);
+        type(By.name("pass"), password);
+        click(By.cssSelector("[value=Login]"));
     }
 
     public void logout() {
-        wd.findElement(By.linkText("Logout")).click();
+        click(By.linkText("Logout"));
     }
 }
