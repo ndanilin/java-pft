02f2d5012fcbe9761061f7cbfcba776f7db857ff
ndanilin <ndanilin87@gmail.com>
2020-06-08 02:07:28 +0300

    добавил сравнение списков при удалении контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,7 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
 
@@ -11,7 +15,16 @@ public class ContactDeletionTests extends TestBase {
             app.getContactHelper().createContact(new ContactData("Иван", "Петров", "45-46-47",
                     "г. Москва, Ленинградский пр-т, д.5, кв.108", "ivan@mail.com", "test"), true);
         }
-        app.getContactHelper().selectContact();
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().selectContact(before.size() - 1);
         app.getContactHelper().submitContactDeletion();
+        app.getNavigationHelper().gotoHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(after, before);
+
+
     }
 }
