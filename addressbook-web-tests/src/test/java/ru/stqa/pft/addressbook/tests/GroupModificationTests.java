9db7e0bc69f4e5aee25e35469961abbfe4c53485
ndanilin <ndanilin87@gmail.com>
2020-06-13 20:34:22 +0300

    5.4 переделал тесты для групп на fluent-интерфейс

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -14,7 +14,7 @@ public class GroupModificationTests extends TestBase {
     public void ensurePreconditions(){
         app.goTo().groupPage();
         if (app.group().list().size() == 0) {
-            app.group().create(new GroupData("test", null, null));
+            app.group().create(new GroupData().withName("test"));
         }
     }
 
@@ -22,7 +22,8 @@ public class GroupModificationTests extends TestBase {
     public void testGroupModification() {
         List<GroupData> before = app.group().list();
         int index = before.size() - 1;
-        GroupData group =new GroupData(before.get(index).getId(), "test2", "testLogo", "testFooter");
+        GroupData group =new GroupData()
+                .withId(before.get(index).getId()).withName("test2").withHeader("testLogo").withFooter("testFooter");
         app.group().modify(index, group);
         List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size());
