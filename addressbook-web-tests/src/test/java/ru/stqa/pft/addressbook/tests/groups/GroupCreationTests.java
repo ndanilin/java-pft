6a4096c4cc673fb129f55effa698a4110b18ab2c
ndanilin <ndanilin87@gmail.com>
2020-06-20 19:50:13 +0300

    6.6 В тест создания групп добавлена работа с xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/groups/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/groups/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests.groups;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,14 +23,18 @@ public class GroupCreationTests extends TestBase {
     @DataProvider
     public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+        StringBuilder xml = new StringBuilder();
         String line = reader.readLine();
         while (line != null){
-            String[] split = line.split(";");
-            list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            xml.append(line);
             line = reader.readLine();
         }
-        return list.iterator();
+        XStream xStream = new XStream();
+        xStream.alias("groups", GroupData.class);
+        xStream.omitField(GroupData.class, "id");
+        List<GroupData> groups = (List<GroupData>) xStream.fromXML(String.valueOf(xml));
+        return groups.stream().map(g -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
     @Test(dataProvider = "validContacts")
