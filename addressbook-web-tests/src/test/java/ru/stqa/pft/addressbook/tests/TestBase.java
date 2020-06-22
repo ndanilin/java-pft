7b916a13116a0ae49ed97810484347da8e47cff6
ndanilin <ndanilin87@gmail.com>
2020-06-22 23:24:42 +0300

    добавил логирование в проект

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+
+import static java.util.Arrays.asList;
 
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -18,8 +27,19 @@ public class TestBase {
 
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method method, Object[] params) {
+        logger.info("Start test " + method.getName() + " with params " + asList(params));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method method) {
+        logger.info("Stop test " + method.getName());
+
     }
 }
