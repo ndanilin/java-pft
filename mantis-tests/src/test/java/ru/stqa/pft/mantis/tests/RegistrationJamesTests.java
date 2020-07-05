f1982a350300352b06ab177548d134b6b29a3a86
ndanilin <ndanilin87@gmail.com>
2020-07-05 22:32:54 +0300

    8.7 добавлен тест на регистрацию с использованием внешнего почтового сервера

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationJamesTests.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static java.lang.String.format;
+import static org.testng.Assert.assertTrue;
+
+public class RegistrationJamesTests extends TestBase {
+
+    @Test
+    public void testRegistration() throws IOException, MessagingException {
+        long now = System.currentTimeMillis();
+        String user = format("user%s", now);
+        String password = "password";
+        String email = format("user%s@localhost.localdomain", now);
+        app.james().createUser(user, password);
+        app.registration().start(user, email);
+        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
+        String confirmationLink = findConfirmationLink(mailMessages, email);
+        app.registration().finish(confirmationLink, user, password);
+        assertTrue(app.newSession().login(user, password));
+    }
+
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+        MailMessage mailMessage = mailMessages.stream().filter(m -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
+
+}
