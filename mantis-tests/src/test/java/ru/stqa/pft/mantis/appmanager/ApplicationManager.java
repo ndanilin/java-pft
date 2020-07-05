0a4e8ab23ebc301d588da6964ebf863759994f23
ndanilin <ndanilin87@gmail.com>
2020-07-05 14:04:13 +0300

    8.4 реализована ленивая инициализация вебдрайвера

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -18,7 +18,8 @@ import static java.lang.String.format;
 public class ApplicationManager {
     private final Properties properties;
     private final String browser;
-    public WebDriver wd;
+    private WebDriver wd;
+    private RegistrationHelper registrationHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -28,31 +29,43 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(format("src/test/resources/%s.properties", target))));
-
-
-        if (browser.equals(BrowserType.FIREFOX)) {
-            FirefoxOptions options = new FirefoxOptions();
-            options.setBinary(properties.getProperty("firefox.binaryPath"));
-            wd = new FirefoxDriver(options);
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
     }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
-    public HttpSession newSession(){
+    public HttpSession newSession() {
         return new HttpSession(this);
     }
 
-    public String getProperty(String key){
+    public String getProperty(String key) {
         return properties.getProperty(key);
     }
 
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                FirefoxOptions options = new FirefoxOptions();
+                options.setBinary(properties.getProperty("firefox.binaryPath"));
+                wd = new FirefoxDriver(options);
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
 }
